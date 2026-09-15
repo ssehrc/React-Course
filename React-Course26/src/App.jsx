cedff06d0bf369a30bd0275f0741adf94b0b4322
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-09-15 12:22:42 -0400

    Applied css to card components

--- React-Course26/src/App.jsx
+++ React-Course26/src/App.jsx
@@ -14,14 +14,7 @@ import './App.css'
 
 const Card = ({title, rating, isCool}) => {
   return (
-    <div className="card" style={{
-      border: '1px solid #4b5362',
-      padding: '20px',
-      margin: '10px',
-      backgroundColor: '#31363f',
-      borderRadius: '10px',
-      minHeight: '100px',
-    }}>
+    <div className="card" >
       <h2>{title}</h2>
     </div>
   )
