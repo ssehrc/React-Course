740ae32abe38a48abc07db87ec47963eae3c1059
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-09-18 13:54:20 -0400

    input the default tailwindcss code

--- React-Course26/src/App.jsx
+++ React-Course26/src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 const App = () => {
-
-  return( 
-    <div className="pattern" />
-    
-    </div>
+  return ( 
+    <main>
+      <div className="pattern" >
+        <div className="wrapper"> 
+          <header>
+            <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle.</h1>
+          </header>
+        </div>
+      </div>
+    </main>
   )
 }
  
-export default App;
+export default App
