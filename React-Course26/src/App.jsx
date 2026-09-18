2b3c3031095208716b8b10d4b48213a8933ed3ef
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-09-18 13:59:17 -0400

    images

--- React-Course26/src/App.jsx
+++ React-Course26/src/App.jsx
@@ -4,13 +4,13 @@ import { useEffect, useState } from "react";
 const App = () => {
   return ( 
     <main>
-      <div className="pattern" >
+      <div className="pattern" />
         <div className="wrapper"> 
           <header>
             <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle.</h1>
           </header>
+          <p>Search</p>
         </div>
-      </div>
     </main>
   )
 }
