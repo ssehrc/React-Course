5511d08f4709edcd390dc74cc1b727cf32a25779
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-09-22 15:10:01 -0400

    integrated movie API and errorhandling

--- React-Course26/src/App.jsx
+++ React-Course26/src/App.jsx
@@ -1,8 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Search from './components/Search.jsx'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3/discover/movie';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async () => {
+    try{
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error('Failed to fetch movies');
+      }
+      const data =await response.json();
+      console.log(data);
+    }catch (error){
+      console.log(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movie. Please try again.');
+    }
+  }
+
+  useEffect(() => {
+
+  }, []);
   return ( 
     <main>
       <div className="pattern" />
@@ -14,6 +48,11 @@ const App = () => {
           </header>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           <h1 className='text-white'>{searchTerm}</h1>
+
+          <section className="all-movies">
+            <h2>All Movies</h2>
+            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          </section>
         </div>
     </main>
   )
