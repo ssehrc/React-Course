25771255f8dde71250fc78664993464f5f8b8f0e
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-09-26 14:39:08 -0400

    Completed movie card design.

--- React-Course26/src/components/MovieCard.jsx
+++ React-Course26/src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ const MovieCard = ({movie: { title, vote_average, poster_path, release_date,
 original_language}}) => {
     return (
         <div className='movie-card'>
-            <img src={poster_path ? `https://image/tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} 
+            <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} 
             alt={title}/>
             <div className='mt-4'>
                 <h3>
@@ -16,6 +16,9 @@ original_language}}) => {
                         <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
                     </div>
                     <span>•</span>
+                    <p className='lang'>{original_language}</p>
+                    <span>•</span>
+                    <p className='year'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
                 </div>
             </div>
         </div>
