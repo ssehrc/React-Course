cf927d27a9902f4abae580e98ca30ac77169ccf1
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-09-26 14:49:38 -0400

    Implemented search bar.

--- React-Course26/src/App.jsx
+++ React-Course26/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import Spinner from './components/Spinner.jsx';
 import Search from './components/Search.jsx'
 import MovieCard from './components/MovieCard.jsx'
 
@@ -20,11 +21,12 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
     try{
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       const data = await response.json();
@@ -44,8 +46,8 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return ( 
     <main>
